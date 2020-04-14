278a8a5327ddf3d49da86b50a218aa403d0d9404
Ben March <bmarch89@gmail.com>
2020-04-14 15:50:09 -0400

    decided to go with react-hooks-testing-library

--- test/useTour.test.js
+++ test/useTour.test.js
@@ -1,29 +1,20 @@
-import { useContext, useRef, useEffect } from 'react'
+import React from 'react'
+import { renderHook } from '@testing-library/react-hooks'
 import { TourContext } from '../lib/TourProvider'
 import useTour from '../lib/useTour'
 
-jest.mock('react')
-
-describe('useTour Hook', () => {
-  it('should use the TourContext', async () => {
-    // when
-    useTour()
-
-    // then
-    expect(useContext).toHaveBeenCalledWith(TourContext)
-  })
-
-  it('should return the tour controller and registerStep function', () => {
+describe('useTour Hook LIB', () => {
+  it('should return the tour controller from context and registerStep function', () => {
     // given
     const tourController = {}
-    useContext.mockReturnValueOnce(tourController)
-    
+    const wrapper = ({ children }) => (
+      <TourContext.Provider value={tourController}>{children}</TourContext.Provider>
+    )
     // when
-    const { tour, registerStep } = useTour()
+    const { result } = renderHook(() => useTour(), { wrapper })
 
-    // then
-    expect(tour).toBe(tourController)
-    expect(registerStep).toEqual(expect.any(Function))
+    expect(result.current.tour).toBe(tourController)
+    expect(result.current.registerStep).toEqual(expect.any(Function))
   })
 
   describe('registerStep()', () => {
@@ -44,64 +35,46 @@ describe('useTour Hook', () => {
         configOnTourOnly: 'tour',
         configOnStepAndTour: 'both - tour',
         getConfig: key => tour[key]
-      }
-      useContext.mockReturnValueOnce(tour)
+      }  
 
-      const { registerStep: rs } = useTour()
-      registerStep = rs
+      const wrapper = ({ children }) => (
+        <TourContext.Provider value={tour}>{children}</TourContext.Provider>
+      )
+      const { result } = renderHook(() => useTour(), { wrapper })
+      registerStep = result.current.registerStep
     })
 
-    it('should create a ref, assign it to the stepConfig, and return it', () => {
+    it('should create a ref, assign it to the stepConfig, and return it', () => {      
       // when
-      const ref = registerStep(stepConfig)
+      const { result } = renderHook(() => registerStep(stepConfig))
 
       // then
-      expect(useRef).toHaveBeenCalledWith(null)
-      expect(stepConfig.ref).toBe(ref)
+      expect(stepConfig.ref).toBe(result.current)
     })
 
     it('should assign a getConfig() method to the stepConfig', () => {
       // when
-      registerStep(stepConfig)
+      renderHook(() => registerStep(stepConfig))
 
-      // then      
+      // then
       expect(stepConfig.getConfig).toEqual(expect.any(Function))
     })
 
-    it('should add the step to the tour in a useEffect hook', () => {
+    it('should add the step to the tour', () => {
       // when
-      registerStep(stepConfig)
-      const effect = useEffect.mock.calls[0][0]
-
-      // then
-      expect(effect).toEqual(expect.any(Function))
-      expect(tour.addStep).not.toHaveBeenCalled()
-      
-      // when
-      effect()
+      renderHook(() => registerStep(stepConfig))
 
       // then
       expect(tour.addStep).toHaveBeenCalledWith(stepConfig)
+      expect(tour.removeStep).not.toHaveBeenCalledWith(stepConfig)
     })
 
-    it('should remove the step from the tour in a useEffect hook', () => {
+    it('should remove the step from the tour on cleanup', () => {
+      // given
+      const { unmount } = renderHook(() => registerStep(stepConfig))
+
       // when
-      registerStep(stepConfig)
-      const effect = useEffect.mock.calls[0][0]
-
-      // then
-      expect(effect).toEqual(expect.any(Function))
-      expect(tour.removeStep).not.toHaveBeenCalled()
-      
-      // when
-      const cleanupEffect = effect()
-
-      // then
-      expect(cleanupEffect).toEqual(expect.any(Function))
-      expect(tour.removeStep).not.toHaveBeenCalled()
-
-      // when 
-      cleanupEffect()
+      unmount()
 
       // then
       expect(tour.removeStep).toHaveBeenCalledWith(stepConfig)
@@ -110,7 +83,7 @@ describe('useTour Hook', () => {
     describe('getConfig()', () => {
       it('should return a config value from the stepConfig if it is defined', () => {
         // when
-        registerStep(stepConfig)
+        renderHook(() => registerStep(stepConfig))
 
         // then      
         expect(stepConfig.getConfig).toEqual(expect.any(Function))
@@ -121,7 +94,7 @@ describe('useTour Hook', () => {
 
       it('should return a config value from the tour if it is defined', () => {
         // when
-        registerStep(stepConfig)
+        renderHook(() => registerStep(stepConfig))
 
         // then      
         expect(stepConfig.getConfig).toEqual(expect.any(Function))
