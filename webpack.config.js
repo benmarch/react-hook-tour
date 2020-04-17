2c669a39909027d1e6b5adf9fd4eb5b5d7b0f4ab
Ben March <bmarch89@gmail.com>
2020-04-17 18:16:50 -0400

    replaced parcel with webpack for production builds

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: './lib/index.js',
+  output: {
+    filename: 'index.js',
+    path: __dirname + '/dist',
+    libraryTarget: 'umd',
+    library: 'reactHookTour',
+    umdNamedDefine: true
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.jsx?$/, 
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
